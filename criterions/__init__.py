8b5dc95c388d1511b8f1aa1bc5d59700c3dbcb51
kiaraesguerra <kiaraesguerra@gmail.com>
2023-09-13 22:41:45 +0800

    Add cross_entropy_loss to options

--- criterions/__init__.py
+++ criterions/__init__.py
@@ -1,7 +1,12 @@
-__all__ = ["bce_loss", "l1_loss", "mse_loss", "focal_loss"]
+__all__ = ["bce_loss",
+           "l1_loss",
+           "mse_loss",
+           "focal_loss",
+           "cross_entropy_loss"]
 
 
 from .bce import bce_loss
 from .l1 import l1_loss
 from .mse import mse_loss
 from .focal import focal_loss
+from .cross_entropy import cross_entropy_loss
