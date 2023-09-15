8717f240296862befea704955230d73d5ec95f03
kiaraesguerra <kiaraesguerra@gmail.com>
2023-09-15 15:12:41 +0800

    Add conditional if args.callbacks/args.exports is None

--- callbacks/callbacks.py
+++ callbacks/callbacks.py
@@ -3,7 +3,10 @@ import callbacks
 
 def get_callback(args):
     defaults = ["checkpoint"]
-    callbacks_name_list = set(args.callbacks + defaults)
+
+    callbacks_name_list = (
+        defaults if args.callbacks is None else set(args.callbacks + defaults)
+    )
 
     callbacks_list = [
         callbacks.__dict__[callback + "_callback"](args)
--- exports/exports.py
+++ exports/exports.py
@@ -3,7 +3,9 @@ import exports
 
 def export_model(model, args):
     defaults = ["torch"]
-    exports_name_list = set(args.exports + defaults)
+    exports_name_list = (
+        defaults if args.exports is None else set(args.exports + defaults)
+    )
 
     for export in exports_name_list:
         exports.__dict__[export + "_export"](model, args)
